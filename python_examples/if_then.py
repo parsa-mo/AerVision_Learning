2aebdf114b1a678f8cacf2d2af26a1ecc08278c5
Parsa <mo.parsa@yahoo.com>
2025-03-12 13:17:31 +1100

    Conditionals 1

---
+++ python_examples/if_then.py
@@ -0,0 +1,10 @@
+# Collect string / test length
+
+
+
+raw_input = input("please enter a test string:")
+
+if len(raw_input) < 6: 
+	print("Your string is too short.")
+	print("Please enter a string at least 6 characters")
+
